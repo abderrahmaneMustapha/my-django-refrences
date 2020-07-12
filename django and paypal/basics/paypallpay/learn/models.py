2a3da0bad6e573332f11e4d834ff74fe06293560
abderrahmaneMustapha <abderrahmanemustapha030898@gmail.com>
2020-07-12 23:27:02 +0100

    setting up models

--- django and paypal/basics/paypallpay/learn/models.py
+++ django and paypal/basics/paypallpay/learn/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    name = models.CharField(("course name"), max_length=50)
+    description = models.TextField(("course description"), max_length=300)
+    price =  models.FloatField(("course price"))
+
+
+class Order(models.Model):
+    user_name = models.CharField(_("username "), max_length=50)
+    adress = models.CharField(_("user adress"), max_length=50)
+
+
