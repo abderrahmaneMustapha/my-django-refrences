96acc786feffbf6bad55df193b3b20c3bdad56e6
abderrahmaneMustapha <abderrahmanemustapha030898@gmail.com>
2020-07-16 10:34:42 +0100

    add client id url

--- django and paypal/basics/paypallpay/learn/urls.py
+++ django and paypal/basics/paypallpay/learn/urls.py
@@ -4,5 +4,6 @@ urlpatterns = [
     path('', index, name="index"),
     path('paypal/pay/<pk>/', pay, name="pay"),
     path('paypal/create/', create, name="paypal-create"),
-    path('paypal/capture/', capture, name="paypal-capture")
+    path('paypal/capture/', capture, name="paypal-capture"),
+     path('paypal/client-id/', getClientId , name="client-id")
 ]
