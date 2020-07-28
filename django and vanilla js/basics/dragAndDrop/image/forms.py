fec113bb94e77c0579471a3f9eb1aec86b6d73eb
abderrahmaneMustapha <abderrahmanemustapha030898@gmail.com>
2020-07-28 11:48:34 +0100

    add form

--- django and vanilla js/basics/dragAndDrop/image/forms.py
+++ django and vanilla js/basics/dragAndDrop/image/forms.py
@@ -1,7 +1,9 @@
-from django import form
+from django import forms
 from .models import ImageModel
 
 class ImageForm(forms.Form):
+    image = forms.ImageField(label="drag and drop an image here")
     class Meta:
         model = ImageModel
+        fields = ('image')
 
--- django and vanilla js/basics/dragAndDrop/image/views.py
+++ django and vanilla js/basics/dragAndDrop/image/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-
+from .forms import ImageForm
 # Create your views here.
 
 def image(request):
-    return render(request, 'image/index.html', {})
+    form = ImageForm()
+    if request.method== "POST":
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valide():
+            form.save()
+        else :
+            data = {'errors': form.errors}
+
+    return render(request, 'image/index.html', {'form':form})
