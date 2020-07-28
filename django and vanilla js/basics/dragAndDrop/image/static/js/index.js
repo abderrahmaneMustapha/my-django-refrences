d8abced954e93926a4e5ed1f91bfb19eeba63243
abderrahmaneMustapha <abderrahmanemustapha030898@gmail.com>
2020-07-28 13:00:29 +0100

    styling and prevent drag and drop events

---
+++ django and vanilla js/basics/dragAndDrop/image/static/js/index.js
@@ -0,0 +1,8 @@
+window.addEventListener("dragover",function(e){
+    e = e 
+    e.preventDefault();
+  },false);
+  window.addEventListener("drop",function(e){
+    e = e 
+    e.preventDefault();
+  },false);
