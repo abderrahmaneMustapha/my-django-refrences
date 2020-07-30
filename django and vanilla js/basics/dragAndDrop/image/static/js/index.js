abd31f78e3c1294bf7d9a6b6626a6f7966fd648c
abderrahmaneMustapha <abderrahmanemustapha030898@gmail.com>
2020-07-30 11:42:35 +0100

    add handle file fucntion

--- django and vanilla js/basics/dragAndDrop/image/static/js/index.js
+++ django and vanilla js/basics/dragAndDrop/image/static/js/index.js
@@ -7,6 +7,20 @@ window.addEventListener("dragover",function(e){
     e.preventDefault();
   },false);
 
+  handle_file = (element,file)=>{
+    const reader = new FileReader();
+    reader.readAsDataURL(file)
+    reader.onload = function fileReadCompleted() {     
+            delete_el(element,'label')
+            delete_el(element,'droped-image')
+            const img = new Image();          // creates an <img> element
+            img.src = reader.result;         // loads the data URL as the image source
+            img.className = "droped-image"
+            element.appendChild(img);   // adds the image to the body        
+        
+    };
+}
+
 
   file_inputs_drop  = document.getElementsByClassName('file-input')
   Array.prototype.forEach.call(file_inputs_drop, element=>{
