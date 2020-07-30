17d043c896fed8f5ef586b97724da66342e8d539
abderrahmaneMustapha <abderrahmanemustapha030898@gmail.com>
2020-07-30 11:42:04 +0100

    add drag and drop fucntion

--- django and vanilla js/basics/dragAndDrop/image/static/js/index.js
+++ django and vanilla js/basics/dragAndDrop/image/static/js/index.js
@@ -6,3 +6,20 @@ window.addEventListener("dragover",function(e){
     e = e 
     e.preventDefault();
   },false);
+
+
+  file_inputs_drop  = document.getElementsByClassName('file-input')
+  Array.prototype.forEach.call(file_inputs_drop, element=>{
+      input=  element.getElementsByTagName('input')[0]
+      element.addEventListener('drop', event=>{
+          handle_file(element,event.dataTransfer.files[0])
+          element.getElementsByTagName('input')[0].files = event.dataTransfer.files
+  
+          console.log(element.getElementsByTagName('input')[0].files)
+      })
+      
+      /// get input inside this element
+      input.addEventListener('change', event=>{
+          handle_file(element,event.target.files[0],)
+      })
+  })
